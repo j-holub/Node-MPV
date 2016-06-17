1606c18055770d1451199aad325abd9bb10a157a
Jan Holub <Jan-Holub@gmx.net>
2016-06-17 02:31:14 +0200

    Basic event handling for playing state

--- mpv.js
+++ mpv.js
@@ -7,6 +7,11 @@ var spawn = require('child_process').spawn;
 
 function mpv(){
 
+	// status object
+	this.status = {
+		'playing': false,
+	}
+
 	// socket file
 	var socketFile = 'mpv.sock';
 
@@ -29,10 +34,35 @@ function mpv(){
 		this.mpvPlayer = spawn('mpv', defaultArgs);
 	});
 	
-
+	// handles the data received from the IPC socket
 	this.socket.on('message', function(data) {
-		console.log(data.toString());
-	});
+		// handle events
+		if(data.event){
+			switch(data.event) {
+				case "idle":
+					this.status.playing = false;
+					console.log("idleidleidle");
+					break;
+				case "playback-restart":
+					this.status.playing = true;
+					console.log("playback");
+					break;
+				case "pause":
+					this.status.playing = false;
+					console.log("pause");
+					break;
+				case "unpause":
+					// TODO when no file is loaded this will break;
+					this.status.playing = true;
+					console.log("pause");
+					break;
+				default:
+					console.log(data);
+			}
+			
+		}
+
+	}.bind(this));
 
 }
 
