cd2d32cc4b7f2da0430daaff61ef8cb4e4c669c8
Jan Holub <Jan-Holub@gmx.net>
2016-06-19 00:38:21 +0200

    The mpv class will now emit the time positon of the currently played song rougly every second as an event

--- mpv.js
+++ mpv.js
@@ -3,6 +3,11 @@ var socket = require('./ipcConnection');
 
 // Child Process to module to start mpv player
 var spawn = require('child_process').spawn;
+// for inheritence purposes
+var util = require('util');
+// EventEmitter
+var eventEmitter = require('events').EventEmitter;
+
 
 
 function mpv(){
@@ -20,7 +25,12 @@ function mpv(){
 		"media-title": null
 	};
 
+	// saves the IDs of observedProperties with their propertyname
+	// key: id  value: property
 	this.observedIDs = {};
+
+	// timeposition of the current song
+	var currentTimePos = null;
 
 	// socket file
 	var socketFile = 'mpv.sock';
@@ -39,11 +49,21 @@ function mpv(){
 	// start mpv instance
 	this.mpvPlayer = spawn('mpv', defaultArgs);
 
+
+	// sets the Interval to emit the current time position
+	this.socket.command("observe_property", [0, "time-pos"]);
+	setInterval(function() {
+		// only emit the time position if there is a file playing and it's not paused
+		if(this.observed.filename && !this.observed.pause && currentTimePos != null){
+			this.emit('timeposition', currentTimePos);
+		}
+	}.bind(this), 980);
+
 	
 
 	// will observe all properties defined in the observed JSON dictionary
 	var observeProperties = function() {
-		var id = 0;
+		var id = 1;
 		// for every property stored in observed
 		Object.keys(this.observed).forEach(function (property) {
 			// safety check
@@ -65,12 +85,16 @@ function mpv(){
 	this.mpvPlayer.on('close', function() {
 		console.log("mpv died, restarting...");
 		this.mpvPlayer = spawn('mpv', defaultArgs);
+		// TODO: reset ALL default parameters
+		currentTimePos = null;
 		// a small timeout is required to wait for mpv to have restarted
 		// on weak machines this could take a while, thus 1000ms
 		setTimeout(function() {
 			// reobserve all observed properties
 			// this will include those added by the user
 			observeProperties();
+			// observe timeposition
+			this.socket.command("observe_property", [0, "time-pos"]);
 		}.bind(this), 1000);
 	}.bind(this));
 
@@ -98,9 +122,18 @@ function mpv(){
 					break;
 				// observed properties
 				case "property-change":
-					this.observed[data.name] = data.data;
-					console.log(`property change ${data.name} ${data.data}`);
-					break;
+					// time position events are handled seperately
+					if(data.name === "time-pos"){
+						// set the current time position
+						currentTimePos = data.data;
+						break;
+					}
+					else{
+						// updates the observed value or adds it, if it was previously unobserved
+						this.observed[data.name] = data.data;
+						console.log(`property change ${data.name} ${data.data}`);
+						break;
+					}
 				default:
 					console.log(data);
 			}
@@ -190,3 +223,16 @@ mpv.prototype = {
 		this.socket.command("unobserve_property", [id]);
 	}
 }
+
+// let mpv inhertid from EventEmitter
+util.inherits(mpv, eventEmitter);
+
+var mpv = new mpv();
+mpv.loadStream("https://www.youtube.com/watch?v=FC3y9llDXuM");
+// mpv.mute();
+
+mpv.observeProperty("time-pos", 10);
+
+mpv.on('timeposition', function(time){
+	console.log("Emitted Position: " + time);
+});
